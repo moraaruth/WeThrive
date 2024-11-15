87b0cd8f3069544b8bf449c11787d7f39d6cc163
Ruth Moraa <moraar37@gmail.com>
2024-11-15 21:36:11 +0300

    adding layout to app folder

---
+++ app/layout.jsx
@@ -0,0 +1,11 @@
+const MainLayout = () => {
+    return ( <html>
+        <body>
+            <main>
+                Main Layout
+            </main>
+        </body>
+    </html> );
+}
+ 
+export default MainLayout;
