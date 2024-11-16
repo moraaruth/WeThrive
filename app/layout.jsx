3638174976e7eab46796eec33dbfe47bbc7da339
Ruth Moraa <moraar37@gmail.com>
2024-11-16 16:32:31 +0300

    adding keywords to metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import '@/assets/styles/globals.css';
 
 export const metadata = {
     title: 'we thrive',
+    keywords: 'autism awareness, autism support, autism resources, autism spectrum disorder, early intervention autism, autism advocacy, special needs support, inclusive education, autism diagnosis, autism services, behavioral therapy, speech therapy autism, autism family support, autism friendly activities, neurodiversity, sensory friendly spaces, autism in children, autism in adults, social skills autism, autism friendly events'
 }
 
 const MainLayout = ({ children }) => {
