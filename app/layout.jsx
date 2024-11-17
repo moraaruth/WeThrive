bc932039ce890c64c88fef22a9d5add1c498c4e6
Ruth Moraa <moraar37@gmail.com>
2024-11-17 05:10:59 +0300

    adding description to metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,8 @@ import '@/assets/styles/globals.css';
 
 export const metadata = {
     title: 'we thrive',
-    keywords: 'autism awareness, autism support, autism resources, autism spectrum disorder, early intervention autism, autism advocacy, special needs support, inclusive education, autism diagnosis, autism services, behavioral therapy, speech therapy autism, autism family support, autism friendly activities, neurodiversity, sensory friendly spaces, autism in children, autism in adults, social skills autism, autism friendly events'
+    keywords: 'autism awareness, autism support, autism resources, autism spectrum disorder, early intervention autism, autism advocacy, special needs support, inclusive education, autism diagnosis, autism services, behavioral therapy, speech therapy autism, autism family support, autism friendly activities, neurodiversity, sensory friendly spaces, autism in children, autism in adults, social skills autism, autism friendly events',
+    description: 'We are unique and yes, we thrive'
 }
 
 const MainLayout = ({ children }) => {
