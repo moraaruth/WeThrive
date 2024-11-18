299d24c229ea23661a52f12066cbfd657addf9cc
Ruth Moraa <moraar37@gmail.com>
2024-11-18 04:35:22 +0300

    adding Navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    // authentication
+    // dropdown
+    //google image
+    // number of images
+    // hamburger menu
+    // active links
+    return ( <div>Navbar</div> );
+}
+ 
+export default Navbar;
