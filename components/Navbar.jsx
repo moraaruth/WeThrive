cf46934f20875e8d26a1bcb725c35ecdce41ec40
Ruth Moraa <moraar37@gmail.com>
2024-11-23 04:46:27 +0300

    adding onClick to the mobile menu button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,7 @@ import profileDefault from "@/assets/images/profile.png";
 import { FaGoogle } from 'react-icons/fa'
 
 const Navbar = () => {
-  const [] = useState
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     // authentication
     // dropdown
     //google imgimg
@@ -27,6 +27,7 @@ const Navbar = () => {
               className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
+              onClick={ () => setIsMobileMenuOpen }
             >
               <span className="absolute -inset-0.5"></span>
               <span className="sr-only">Open main menu</span>
