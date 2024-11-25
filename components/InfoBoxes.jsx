23d081861979a28e5ec93e60db12c55b810f8fee
Ruth Moraa <moraar37@gmail.com>
2024-11-25 04:32:33 +0300

    adding dynamic InfoBoxes

--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -1,5 +1,17 @@
+import InfoBox from "./InfoBox";
 const InfoBoxes = () => {
-    return ( <div>Info boxes</div> );
+    return ( 
+        <section>
+        <div className="container-xl lg:container m-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+<InfoBox heading="Therapist"> Find a therapist around you. Bookmark therapist and contact.</InfoBox>
+<InfoBox heading="Therapy session" backgroundColor="bg-blue-100"> Check the available therapy sessions and book.</InfoBox>
+
+            
+          </div>
+        </div>
+      </section>
+    );
 }
  
 export default InfoBoxes;
