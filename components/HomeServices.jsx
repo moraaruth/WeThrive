9e0018b82e3289af3aeeac5570a362ba3a806371
Ruth Moraa <moraar37@gmail.com>
2024-11-26 06:04:55 +0300

    adding heading to services offered

--- components/HomeServices.jsx
+++ components/HomeServices.jsx
@@ -1,5 +1,24 @@
+import services from '@/services.json';
+import ServiceCard from './ServiceCard';
+
+
 const HomeServices = () => {
-    return ( <div>Home</div> );
+    return ( 
+        <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+           <h2 className = "text-3xl font-bold text-blue-500 mb-6 text-center">Services Available</h2>
+          {services.length === 0 ? (
+            <p>No service found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {services.map((service) => (
+                <ServiceCard  key={service._id} service={service}/>
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+        );
 }
  
 export default HomeServices;
