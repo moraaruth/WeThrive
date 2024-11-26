61e3b0140bf523b99e7e2a98efb09e569ad52da1
Ruth Moraa <moraar37@gmail.com>
2024-11-26 05:56:45 +0300

    adding Homeservices component to homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,12 @@
 import Hero from "@/components/Hero";
+import HomeServices from "@/components/HomeServices";
 import InfoBoxes from "@/components/InfoBoxes";
 const HomePage = () => {
     return ( 
     <>
         <Hero />
         <InfoBoxes />
-        
+        <HomeServices />
         </> 
         );
 }
