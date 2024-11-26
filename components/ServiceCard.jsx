1a05eeebf4e2e099bda37d13befd0bc2656a2756
Ruth Moraa <moraar37@gmail.com>
2024-11-26 05:28:52 +0300

    adding dynamic details to serviceCard

--- components/ServiceCard.jsx
+++ components/ServiceCard.jsx
@@ -14,31 +14,16 @@ const ServiceCard = ({service}) => {
         />
         <div className="p-4">
           <div className="text-left md:text-center lg:text-left mb-6">
-            <div className="text-gray-600">Apartment</div>
-            <h3 className="text-xl font-bold">Boston Commons Retreat</h3>
+            <div className="text-gray-600">{service.type}</div>
+            <h3 className="text-xl font-bold">{service.name}</h3>
           </div>
           <h3
             className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right"
           >
-            $4,200/mo
+            ksh2,200/mo
           </h3>
 
-          <div className="flex justify-center gap-4 text-gray-500 mb-4">
-            <p>
-              <i className="fa-solid fa-bed"></i> 3
-              <span className="md:hidden lg:inline">Beds</span>
-            </p>
-            <p>
-              <i className="fa-solid fa-bath"></i> 2
-              <span className="md:hidden lg:inline">Baths</span>
-            </p>
-            <p>
-              <i className="fa-solid fa-ruler-combined"></i>
-              1,500 <span className="md:hidden lg:inline">sqft</span>
-            </p>
-          </div>
-
-          <div
+                 <div
             className="flex justify-center gap-4 text-green-900 text-sm mb-4"
           >
             <p><i className="fa-solid fa-money-bill"></i> Weekly</p>
@@ -52,14 +37,14 @@ const ServiceCard = ({service}) => {
               <i
                 className="fa-solid fa-location-dot text-lg text-orange-700"
               ></i>
-              <span className="text-orange-700"> Boston MA </span>
+              <span className="text-orange-700"> {service.location.constituency} {service.location.city} </span>
             </div>
-            <a
-              href="property.html"
+            <Link
+              href={`/services/${service._id}`}
               className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
             >
               Details
-            </a>
+            </Link>
           </div>
         </div>
       </div>
