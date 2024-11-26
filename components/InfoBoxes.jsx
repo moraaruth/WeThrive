1a28ac08b736373bd2768faa945be2500e1dbf0a
Ruth Moraa <moraar37@gmail.com>
2024-11-26 03:45:19 +0300

    formatting

--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -1,25 +1,32 @@
-import InfoBox from "./InfoBox";
+import InfoBox from './InfoBox'
 const InfoBoxes = () => {
-    return ( 
-        <section>
-        <div className="container-xl lg:container m-auto">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-<InfoBox 
-heading="Therapist" 
-buttonInfo=
-{{ 
-    text: 'Find a Therapist',
-    link: '/therapist',
-    backgroundColor: 'bg-black'
-    }}
-    > Find a therapist around you. Bookmark therapist and contact.</InfoBox>
-<InfoBox heading="Therapy session" buttonInfo={{ text: 'Book a Session'}} backgroundColor="bg-blue-100"> Check the available therapy sessions and book.</InfoBox>
-
-            
-          </div>
+  return (
+    <section>
+      <div className="container-xl lg:container m-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+          <InfoBox
+            heading="Therapist"
+            buttonInfo={{
+              text: 'Find a Therapist',
+              link: '/therapist',
+              backgroundColor: 'bg-black',
+            }}
+          >
+            {' '}
+            Find a therapist around you. Bookmark therapist and contact.
+          </InfoBox>
+          <InfoBox
+            heading="Therapy session"
+            buttonInfo={{ text: 'Book a Session' }}
+            backgroundColor="bg-blue-100"
+          >
+            {' '}
+            Check the available therapy sessions and book.
+          </InfoBox>
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  )
 }
- 
-export default InfoBoxes;
+
+export default InfoBoxes
