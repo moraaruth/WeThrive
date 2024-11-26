ebed5152f67ccb6302099b20899b8e4e31a3aded
Ruth Moraa <moraar37@gmail.com>
2024-11-26 04:48:05 +0300

    adding and confirming services are being displayed

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,16 +1,21 @@
-import services from "@/services.json"
+import services from '@/services.json'
 
 const Services = () => {
- 
-    return (
-        <section className="px-4 py-6">
-            <div className="container-xl lg:container m-auto px-4 py-6">
-
-            </div>
-
-        </section>
-     );
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {services.length === 0 ? (
+          <p>No service found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {services.map((service) => (
+              <div>{service.name}</div>
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  )
 }
- 
-export default Services;
 
+export default Services
