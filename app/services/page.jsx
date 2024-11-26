5ce03ff23c8d733981da8d622a1a1ef34000f698
Ruth Moraa <moraar37@gmail.com>
2024-11-26 04:43:28 +0300

    adding container

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -2,7 +2,14 @@ import services from "@/services.json"
 
 const Services = () => {
  
-    return (<div>Services</div>  );
+    return (
+        <section className="px-4 py-6">
+            <div className="container-xl lg:container m-auto px-4 py-6">
+
+            </div>
+
+        </section>
+     );
 }
  
 export default Services;
