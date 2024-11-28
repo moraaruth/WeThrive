d19a13dcec70be7da2f9bb878596e4e41594a9c2
Ruth Moraa <moraar37@gmail.com>
2024-11-28 06:50:41 +0300

    adding cliploader

---
+++ app/loading.jsx
@@ -0,0 +1,8 @@
+'use client';
+import  ClipLoader  from 'react-spinners/ClipLoader';
+
+const LoadingPage = () => {
+    return ( <div> <ClipLoader color="#3b82f6" /></div> );
+}
+ 
+export default LoadingPage;
