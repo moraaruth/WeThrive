ba8e68f6912f9a12afd9aa3adda7a7a68d978046
Ruth Moraa <moraar37@gmail.com>
2024-12-01 06:56:42 +0300

    adding blog schema

--- models/Blog.js
+++ models/Blog.js
@@ -10,6 +10,42 @@ const BlogSchema = new Schema({
     content: {
         type: String,
         required: true
-    }
+    },
+    author: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    tags: [
+        {
+            type: String,
+            trim: true,
+        },
+    ],
+    category: {
+        type: String,
+        enum: ['Therapy', 'Resources', 'Personal stories', 'News', 'Advocacy'],
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: false,
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
 
-})
+});
+
+const Blog = models.Blog || model('Blog', BlogSchema);
+
+export default Blog;
