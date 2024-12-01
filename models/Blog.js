daf000708cb75a001be06b898fd5fe2dc8eec35a
Ruth Moraa <moraar37@gmail.com>
2024-12-01 06:47:28 +0300

    adding title and content to blog schema

--- models/Blog.js
+++ models/Blog.js
@@ -1,5 +1,15 @@
 import { Schema, model, models } from 'mongoose';
 
 const BlogSchema = new Schema({
-    
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        
+    },
+    content: {
+        type: String,
+        required: true
+    }
+
 })
