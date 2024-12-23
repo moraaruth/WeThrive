c084d49c63adcbbc1444a10e73af03ec83a2c324
Ruth Moraa <moraar37@gmail.com>
2024-12-24 00:02:44 +0300

    adding vision

--- components/AboutusCard.jsx
+++ components/AboutusCard.jsx
@@ -1,26 +1,25 @@
-"use client"
+'use client'
 import React from 'react'
 
 export default function AboutusCard() {
   return (
-   <>
-         <section className="bg-blue-700 py-10 mb-4">
-        <div
-          className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
-        >
+    <>
+      <section className="bg-blue-700 py-10 mb-4">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
           <div className="text-center">
-            <h2
-              className="text-4xl font-extrabold text-white sm:text-3xl md:text-4xl"
-            >
-             About Wethrive
+            <h2 className="text-4xl font-extrabold text-white sm:text-3xl md:text-4xl">
+              About Wethrive
             </h2>
             <p className="my-4 text-xl text-white">
-            Thriving Together: Advocacy, Awareness, and Action for Autism
+              Thriving Together: Advocacy, Awareness, and Action for Autism
             </p>
           </div>
-         
+        </div>
+        <div>
+            <h1>Our Vision</h1>
+            <p>We envision a world where every individual with autism has the opportunity to live a fulfilling and meaningful life.</p>
         </div>
       </section>
-   </>
+    </>
   )
 }
