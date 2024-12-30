991d0e51248029ca85560391cbac10fb042c2f77
Ruth Moraa <moraar37@gmail.com>
2024-12-30 16:06:07 +0300

    adding clientSecret

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -4,6 +4,8 @@ export const authOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+
         })
     ]
 }
