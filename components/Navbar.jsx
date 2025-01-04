a9c33d7fa4346e41ae8450d54e67138d3563c86f
Ruth Moraa <moraar37@gmail.com>
2025-01-04 05:08:51 +0300

    mapping through providers

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -152,11 +152,15 @@ const Navbar = () => {
               !session && (            
             <div className="hidden md:block md:ml-6">
               <div className="flex items-center">
+                {providers && 
+                Object.values(providers).map((provider) => (
                 <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
                   <FaGoogle className="text-white mr-2" />
 
                   <span>Login or Register</span>
                 </button>
+                ))}
+                
               </div>
             </div>
               )}
